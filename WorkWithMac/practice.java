90a46d3396bd1bf779db360e0c4a2777cdd41d4e
utkubarancavus <utkubarancavus@icloud.com>
2022-07-05 19:31:12 -0500

    my message

--- WorkWithMac/practice.java
+++
@@ -1,7 +0,0 @@
-package WorkWithMac;
-
-public class practice {
-    public static void main(String[] args) {
-        System.out.println("hsgadkj");
-    }
-}
---
+++ workwithmac/pra.java
@@ -0,0 +1,7 @@
+package workwithmac;
+
+public class pra {
+    public static void main(String[] args) {
+        System.out.println("jahds");
+    }
+}
